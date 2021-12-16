e6eb741da3f111f80b470b4c690ef6cc5688809c
raoqingjia <1007804453@qq.com>
2021-12-17 00:25:35 +0800

    增加line-height2.0

--- src/views/css/lineHeight.vue
+++ src/views/css/lineHeight.vue
@@ -11,6 +11,8 @@
           <img src="../../img/css/lineheight01.png" height="350px" width="750px">
           <pre>当line-height再变大时，变大的不仅仅是基线，行距也在跟着变化，行距 = line-height - font-size; 半行距 =（line-height - font-size）/2</pre>
           <img src="../../img/css/lineheight02.png" height="350px" width="750px">
+          <pre>line-height与font-size数值大小之间的对比效果</pre>
+          <img src="../../img/css/lineheight05.png" height="350px" width="750px">
           <pre>line-height的垂直居中与height密切相关</pre>
           <img src="../../img/css/lineheight03.png" height="350px" width="750px">
           <img src="../../img/css/lineheight04.png" height="350px" width="750px">
