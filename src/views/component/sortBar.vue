d3dd0fff1b528f880e1fe61b12588829d5205e58
raoqingjia <1007804453@qq.com>
2021-12-17 18:40:36 +0800

    修改dashboard

---
+++ src/views/component/sortBar.vue
@@ -0,0 +1,108 @@
+<template>
+  <div :style='restStyle'>
+    <h3 :model="title"></h3>
+    <ul>
+      <li v-for="(items,index) in list" :key="index">
+        <p class="txt">
+          <span class="fl">{{items.name}}</span>
+          <span class="fr">{{items.data}}</span>
+        </p>
+        <dl class="bar">
+          <dt :style="{width:setWidth()}" class="date-bg"></dt>
+          <dd class="base-bg"></dd>
+        </dl>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'sortBar',
+    data() {
+      return {
+        current: this.currentPage
+      }
+    },
+    props: {
+      list: {
+        type: Array,
+        default: []
+      },
+      restStyle: { // 重置样式
+        type: Object,
+        default: 0
+      },
+      title: {
+        type: String,
+        default: ''
+      },
+      display: {// 每页显示条数
+        type: Number,
+        default: 0
+      }
+    },
+    computed: {
+      setWidth () {
+        return  '80%';
+      },
+    },
+    created() {
+
+    },
+    mounted() {
+    },
+    methods: {}
+  }
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  @height: 16px;
+  ul {
+    width: 100%;
+    padding: 15px 0;
+    li {
+      margin: 0 0 8px 0;
+      padding: 0 20px;
+      p.txt {
+        span {
+          display: block;
+          font-size: 14px;
+          color: #c6ccd8;
+          line-height: 36px;
+        }
+      }
+
+      .bar {
+        width: 100%;
+        height: 12px;
+        overflow: hidden;
+        position: relative;
+        z-index: 1;
+      }
+
+      .date-bg {
+        height: 100%;
+        width: 60%;
+        border-radius: 6px;
+        background: linear-gradient(to right, #eed180, #e95d58);
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: 3;
+      }
+
+      .base-bg {
+        height: 100%;
+        width: 100%;
+        border-radius: 6px;
+        background-color: #fef7f1;
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: 2;
+      }
+    }
+  }
+
+
+</style>
--- src/views/osDashboard.vue
+++ src/views/osDashboard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="dh-wrap">
+  <div class="dh-wrap clearfix">
     <h1 class="dh-title clearfix">
-    OneSupport 运营数据统计
+      OneSupport 运营数据统计
       <section class="dh-date clearfix">
         <label>数据月份：</label>
         <div class="choose-date">
@@ -16,27 +16,44 @@
         <span class="sure-btn">确认</span>
       </section>
     </h1>
-    <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
-    <div id="LiquidRing" :style="{width: '300px', height: '300px'}"></div>
+    <div class="dh-content clearfix">
+      <aside class="fl-chart">
+        <div id="mzptBar" class="chart-item" :style="{height: '260px'}"></div>
+        <div class="chart-item">
+          <sortBar :list="applicationList" :display="0" :title="'今日提供方请求量排行榜'" :restStyle="sortBarStyle"></sortBar>
+        </div>
+      </aside>
+      <section class="middle-chart">
+
+      </section>
+      <aside class="fr-chart">
+        <div id="tgfcgydzb" class="chart-item" :style="{height: '330px'}"></div>
+        <div id="zqstnl" class="chart-item" :style="{height: '330px'}"></div>
+      </aside>
+    </div>
   </div>
 </template>
 
 <script>
-  import {routes} from '@/router'
-  import {mapGetters} from 'vuex'
-  import * as echarts from 'echarts'
-  import pagination from '@/views/component/pagination'
+  import {routes} from '@/router';
+  import * as echarts from 'echarts';
+  import sortBar  from '@/views/component/sortBar';
 
   export default {
     name: 'osDashboard',
     created() {
 
     },
-    components: {},
+    components: {'sortBar': sortBar},
     data() {
       return {
         routes: routes,
-        datePicker: ''
+        datePicker: '',
+        sortBarStyle:{},
+        chartFontColor: '#c6ccd8',
+        chartBackgroundColor: 'rgba(31,36,64,0)',
+        applicationList:[{name:"应用数量1", data:1200},{name:"应用数量1", data:1100},{name:"应用数量1", data:1000},{name:"应用数量1", data:990},{name:"应用数量1", data:800},
+          {name:"应用数量1", data:780},{name:"应用数量1", data:720},{name:"应用数量1", data:690},{name:"应用数量1", data:680},{name:"应用数量1", data:550}]
       }
     },
     mounted() {
@@ -46,59 +63,192 @@
         let scaleY = document.body.clientHeight / 1080;
         let scale = scaleX < scaleY ? scaleX : scaleY;
         let screenWidthStyle = `width: 1920px;height: 1080px;transform: scale(${scale},${scale});!important;margin:0 ${(document.body.clientWidth - 1920 * scale) / 2}px;`;
-        console.log(screenWidthStyle);
-        this.drawLine();
+        this.mzptBar();
+        this.zqstnl(56.8);
+        this.tgfcgydzb(89);
       })
     },
-    filters: {
-      filterFun(value) {
-        return value;
-      }
-    },
     methods: {
-      LiquidRing() {
-        var value = 0.54;
-        let LiquidRing = echarts.init(document.getElementById('LiquidRing'));
-        LiquidRing.setOption({
-          backgroundColor: '#181b22',
-          title: [
-            {
-              text: (value * 100).toFixed(0) + '%',
-              left: '50%',
-              top: '40%',
-              textAlign: 'center',
-              textStyle: {
-                fontSize: '30',
-                fontWeight: '400',
-                color: '#8b8d90',
-                textAlign: 'center',
-                textBorderColor: 'rgba(0, 0, 0, 0)',
-                textShadowColor: '#000',
-                textShadowBlur: '0',
-                textShadowOffsetX: 0,
-                textShadowOffsetY: 1,
-              },
-            },
-          ],
-          polar: {
-            radius: ['43%', '40%'],
-            center: ['50%', '50%'],
+      // 每周平台请求情况
+      mzptBar() {
+        let yList = [32, 58, 64, 64, 64];
+        let xData = ['第一周', '第二周', '第三周', '第四周', '第五周'];
+        let dom = 340;
+        let barWidth = dom / 20;  // 柱状图的宽度
+        let colors = []
+        for (let i = 0; i < xData.length; i++) {
+          colors.push({
+            type: 'linear',
+            x: 0,
+            x2: 1,
+            y: 0,
+            y2: 0,
+            colorStops: [
+              {
+                offset: 0,
+                color: '#73fcff' // 最左边
+              }, {
+                offset: 0.5,
+                color: '#86eef1' // 左边的右边 颜色
+              }, {
+                offset: 0.5,
+                color: '#5ad6d9' // 右边的左边 颜色
+              }, {
+                offset: 1,
+                color: '#3dc8ca'
+              }]
+          })
+        }
+        let option = {
+          title: {
+            text: '  每周平台请求量情况',
+            textStyle: {
+              color: this.chartFontColor,
+              fontSize: 16,
+              lineHeight: 44,
+            }
           },
-          angleAxis: {
-            max: 100,
-            clockwise: false,
+          //提示框
+          tooltip: {
+            trigger: 'axis',
+            formatter: "{b} : {c}",
+            axisPointer: { // 坐标轴指示器，坐标轴触发有效
+              type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+            }
+          },
+          /**区域位置*/
+          grid: {
+            left: '14%',
+            right: '8%',
+            top: '23%',
+            bottom: '18%',
+          },
+          //X轴
+          xAxis: {
+            data: xData,
+            type: 'category',
             axisLine: {
               show: false,
-            },
-            axisTick: {
-              show: false,
-            },
-            axisLabel: {
-              show: false,
+              lineStyle: {
+                color: 'rgba(255,255,255,1)',
+                shadowColor: 'rgba(255,255,255,1)',
+                shadowOffsetX: '20',
+              },
+              symbol: ['none', 'arrow'],
+              symbolOffset: [0, 25]
             },
             splitLine: {
               show: false,
             },
+            axisTick: {
+              show: false
+            },
+            axisLabel: {
+              margin: 15,   // 字体距离x周之间的距离
+              fontSize: 12,  // x轴字体大小
+            },
+          },
+          yAxis: {
+            show: true,
+            splitNumber: 4,
+            axisLine: {
+              show: false,
+            },
+            splitLine: {  //  y轴
+              show: true,
+              lineStyle: {
+                type: 'border',
+                color: '#323d64',
+              },
+            },
+            axisLabel: {
+              color: this.chartFontColor,  //  字体颜色
+              margin: 15,
+              fontSize: 12,  // y轴字体大小
+            }
+          },
+          series: [
+            {
+              type: 'bar',
+              barWidth: barWidth,
+              itemStyle: {
+                normal: {
+                  color: function (params) {
+                    return colors[params.dataIndex % 7];
+                  }
+                }
+              },
+              label: {
+                show: true,
+                position: [barWidth / 2, -(barWidth + 20)],
+                color: this.chartFontColor,  //  字体颜色
+                fontSize: 14,
+                align: 'center',
+              },
+              data: yList
+            },
+            {
+              z: 2,
+              type: 'pictorialBar',
+              data: yList,
+              symbol: 'diamond',
+              symbolOffset: [0, '50%'],
+              symbolSize: [barWidth, barWidth * 0.5],
+              itemStyle: {
+                normal: {
+                  color: function (params) {
+                    return colors[params.dataIndex % 7];
+                  },
+                }
+              },
+            },
+            {
+              z: 3,
+              type: 'pictorialBar',
+              symbolPosition: 'end',
+              data: yList,
+              symbol: 'diamond',
+              symbolOffset: [0, '-50%'],
+              symbolSize: [barWidth, barWidth * 0.5],
+              itemStyle: {
+                normal: {
+                  borderWidth: 0,
+                  color: function (params) {
+                    return colors[params.dataIndex % 7].colorStops[0].color;
+                  },
+
+                }
+              },
+            },
+          ],
+        }
+
+        let myChart = echarts.init(document.getElementById('mzptBar'));
+        myChart.setOption(option);
+
+      },
+      zqstnl(currentDate){
+        let myChart = echarts.init(document.getElementById('zqstnl'));
+        let num = 0;
+        let option = {
+          title: [ {
+            text: currentDate+'%',
+            x: 'center',
+            top: '46%',
+            textStyle: {
+              fontSize: '25',
+              color: '#fdf914',
+              fontFamily: 'Lato',
+              foontWeight: '600',
+            },
+          }],
+          polar: {
+            radius: ['44%', '50%'],
+            center: ['50%', '50%'],
+          },
+          angleAxis: {
+            max: 100,
+            show: false,
           },
           radiusAxis: {
             type: 'category',
@@ -108,127 +258,174 @@
             },
             axisLine: {
               show: false,
+
             },
             axisTick: {
-              show: false,
+              show: false
             },
           },
           series: [
-            {
-              type: 'liquidFill',
-              radius: '40%',
-              z: 1,
-              center: ['50%', '50%'],
-              amplitude: 20,
-              color: {
-                type: 'linear',
-                x: 0,
-                y: 0,
-                x2: 1,
-                y2: 1,
-                colorStops: [
-                  {
-                    offset: 0,
-                    color: '#324791',
-                  },
-                  {
-                    offset: 1,
-                    color: '#449090',
-                  },
-                ],
-                globalCoord: false,
-              },
-              data: [
-                0.4,
-                {
-                  value: 0.4,
-                  direction: 'left',
-                },
-              ],
-              backgroundStyle: {
-                borderWidth: 1,
-                color: 'transparent',
-              },
-              label: {
-                normal: {
-                  formatter: '',
-                },
-              },
-              outline: {
-                show: true,
-                itemStyle: {
-                  borderWidth: 0,
-                },
-                borderDistance: 0,
-              },
-            },
-            {
+            {    // 真实的数据圆圈
               name: '',
               type: 'bar',
               roundCap: true,
-              z: 2,
+              barWidth: 60,
               showBackground: true,
               backgroundStyle: {
-                color: '#15181e',
+                color: 'rgba(66, 66, 66, 0.5)',   //  真实数据圆圈的底色
               },
-              data: [75],
+              data: [currentDate],
               coordinateSystem: 'polar',
               itemStyle: {
                 normal: {
-                  color: new echarts.graphic.LinearGradient(0, 0, .5, 1, [
-                    {
-                      offset: 0,
-                      color: '#5acef2',
-                    },
-                    {
-                      offset: .7,
-                      color: '#5073fb',
-                    },
+                  color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+                    offset: 0,
+                    color: '#fdf914'
+                  }, {
+                    offset: 1,
+                    color: '#38a700'
+                  }]),
+                }
+              }
+            },
+            { //  最外圈的圆
+              name: '',
+              type: 'pie',
+              startAngle: 90,
+              radius: ['60.5%', '64%'],
+              hoverAnimation: false,
+              center: ['50%', '50%'],
+              itemStyle: {
+                normal: {
+                  labelLine: {
+                    show: false
+                  },
+                  color: 'rgba(66, 66, 66, 0.3)',
+                  shadowBlur: 10,
+                  shadowColor: 'rgba(253, 249, 20, 0.3)',
+                }
+              },
+              data: [{value: 100}]
+            },
+            {  //  运动小卫星
+              name: "",
+              type: 'custom',
+              coordinateSystem: "none",
+              renderItem: function(params, api) {
+                return {
+                  type: 'arc',
+                  shape: {
+                    cx: api.getWidth() / 2,
+                    cy: api.getHeight() / 2,
+                    r: Math.min(api.getWidth(), api.getHeight()) / 2 * 0.55, //  运动小卫星的半径
+                    startAngle: (0+num) * Math.PI / 180,
+                    endAngle: (1+num) * Math.PI / 180
+                  },
+                  style: {
+                    stroke: "#fdf914",
+                    fill: "transparent",
+                    lineWidth: 5
+                  },
+                  silent: true
+                };
+              },
+              data: [0]
+            },
+          ]
+        };
+        function numb(){
+          num = num+5
+          myChart.setOption(option, true)
+        }
+        setInterval(function() {
+          numb()
+        }, 100);
+        myChart.setOption(option);
+      },
+      tgfcgydzb(currentDate){
+        let myChart = echarts.init(document.getElementById('tgfcgydzb'));
+        myChart.setOption({
+          backgroundColor: this.chartBackgroundColor,
+          title: [ {
+            text: currentDate+'%',
+            x: 'center',
+            top: '46%',
+            textStyle: {
+              fontSize: '25',
+              color: '#fff',
+              fontFamily: 'Lato',
+              foontWeight: '600',
+            },
+          }],
+          polar: {
+            radius: ['43%', '50%'],
+            center: ['50%', '50%'],
+          },
+          angleAxis: {
+            max: 100,
+            show: false,
+          },
+          radiusAxis: {
+            type: 'category',
+            show: true,
+            axisLabel: {
+              show: false,
+            },
+            axisLine: {
+              show: false,
+
+            },
+            axisTick: {
+              show: false
+            },
+          },
+          series: [
+            {    // 真实的数据圆圈
+              name: '',
+              type: 'bar',
+              roundCap: true,
+              barWidth: 80,
+              showBackground: true,
+              backgroundStyle:{
+                color: 'rgba(231, 216, 236, 0.3)'   //  真实数据圆圈的底色
+              },
+              data: [currentDate],
+              coordinateSystem: 'polar',
+              itemStyle: {
+                normal: {
+                  color: new echarts.graphic.LinearGradient(1,0, 0,  0, [
+                  {
+                    offset: 0,
+                    color: '#8a34e3'
+                  },
+                  {
+                    offset: 0.5,
+                    color: '#d571f9'
+                  },
                     {
                       offset: 1,
-                      color: '#6ae8d8',
-                    },
+                      color: '#c53ff5'
+                    }
                   ]),
-                },
-              },
-            },
-          ],
-        });
-      },
-      drawLine() {
-        console.log(echarts);
-        // 基于准备好的dom，初始化echarts实例
-        let myChart = echarts.init(document.getElementById('myChart'));
-        // 绘制图表
-        myChart.setOption({
-          title: {text: '在Vue中使用echarts'},
-          tooltip: {},
-          xAxis: {
-            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-          },
-          yAxis: {},
-          series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-          }]
+                }
+              }
+            }
+          ]
         });
       }
     },
-
-    computed: {
-      caseNav() {
-        return this.$store.state.caseNav
-      },
-    }
+    computed: {}
   }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped  lang="less">
+<style scoped lang="less">
+
+  @fontColor: '#c6ccd8';
+
   .dh-wrap {
-    margin:-8px 0 0 0;
-    /*background-color:rgba(55,60,107,0.8);*/
-     background-color: #253562;
+    margin: -8px 0 0 0;
+    padding: 0 20px;
+    background-color: #253562;
   }
 
   .dh-title {
@@ -239,38 +436,72 @@
     font-size: 24px;
     font-weight: bold;
     position: relative;
+
     .dh-date {
       position: absolute;
       top: 0;
       right: 50px;
       font-size: 14px;
       font-weight: normal;
-      color: #acb1bf;
+      color: @fontColor;
+
       label {
         float: left;
       }
+
       div.choose-date {
         float: left;
         margin: 0 10px 0 10px;
+
         input[type='text'] {
           height: 30px;
           line-height: 30px;
           width: 150px;
-          background: #192b4f!important;
-          border: 1px solid #192b4f!important;
-          color: #acb1bf!important;
+          background: #192b4f !important;
+          border: 1px solid #192b4f !important;
+          color: @fontColor !important;
         }
       }
+
       span.sure-btn {
         float: left;
         height: 30px;
         line-height: 30px;
         margin: 15px 0 0 0;
         padding: 0 10px;
-        border-radius:4px ;
+        border-radius: 4px;
         background-color: #08203d;
       }
     }
+  }
+
+  .dh-content {
+    margin: 20px 0 0 0;
+  }
+
+  .fl-chart {
+    float: left;
+    width: 30%;
+  }
+
+  .middle-chart {
+    float: left;
+    width: 40%;
+    padding: 0 20px;
+  }
+
+  .fr-chart {
+    float: right;
+    width: 30%;
+  }
+
+  .chart-item {
+    width: 100%;
+    float: left;
+    margin: 0 0 10px 0;
+    border-radius: 4px;
+    background-color: rgba(31,36,64,0.5);
+    border: 2px solid #141e36;
   }
 
 
