29a7366191c37e72184c700a8ae069ca9cb8e134
raoqingjia <1007804453@qq.com>
2022-06-11 22:40:23 +0800

    更新瀑布图

--- src/views/project.vue
+++ src/views/project.vue
@@ -1,42 +1,16 @@
 <template>
-  <div class="base inner">
-    <vue-waterfall-easy  :imgsArr="imgsArr" @scrollLoadImg="fetchImgsData">
+  <div class="base waterfall-wrap inner">
+    <vue-waterfall-easy v-if="imgsArr.length" :imgsArr="imgsArr" @scrollLoadImg="fetchImgsData"  @click="clickFn" :height="'100vh'">
       <template slot-scope="props">
-        <h3>{{props.value.title}}</h3>
-        <p>{{props.value.info}}</p>
+        <div class="img-info">
+          <h3>{{props.value.title}}</h3>
+          <p>{{props.value.info}}</p>
+        </div>
       </template>
     </vue-waterfall-easy>
-    <div class="content" v-if="false">
-      <h2>模板</h2>
-      <ul class="clearfix template">
-        <li>
-          <a href="../../static/ecWebTest/src/app/index.html" target="_blank">
-            <img src="../../static/img/ecweb.jpg">
-            <div class="text">
-              <h3>基于angular-1.2.9搭建</h3>
-              <p>
-                兼容IE8、利用Bootstrap实现响应式、BootstrapDatepicker日期插件、Echart图表、
-                DataTables表单排序、kkpager页面排序功能
-              </p>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="javascript:;">
-            <img src="../img/activemq-icon.jpg">
-            <div class="text">
-              <h3>apache</h3>
-              <p>apache安装、参数配置</p>
-            </div>
-          </a>
-        </li>
-        <!--<li v-for="items in caseNav"><a href="javascript:;"    @click="urlFun(items.code)" v-html="items.text"></a></li>-->
-      </ul>
-    </div>
   </div>
 </template>
 <script>
-  import {routes} from "@/router"
   import vueWaterfallEasy from 'vue-waterfall-easy'
 
   export default {
@@ -45,25 +19,20 @@
     data (){
       return {
         onerror: 'this.src="' + require('@/img/404.jpg') + '"',
-        imgsArr: [   //数组数量小于5个也就是小于一行的数量，会报Cannot read property 'style' of undefined错误
-          {src:'./static/img/ecweb.jpg',link:"http://www.raoqingjia.online/ecWebTest/src/app/index.html",title:"angular1.0项目",info:"基于angular-1.2.9搭建，兼容IE8、利用Bootstrap实现响应式、BootstrapDatepicker日期插件、Echart图表、DataTables表单排序、kkpager页面排序功能"},
-          {src:'./static/img/salss-app.jpg',link:"http://www.raoqingjia.online/salssVue/index.html",title:"H5移动端项目",info:"基于Vue2.0,利用vue全家桶,未使用UI框架，利用rem换算比例适应各移动平台,mobileSelect筛选插件,swiper3.0滑屏,Echart图表"},
-          {src:'./static/img/bpm_web_new.jpg',link:"http://www.raoqingjia.online/bpm_web_new/flow.html",title:"流程图",info:"设计流程图（raphael）源码myflow.js，修改了删除方式，自定义了流程步骤，保存绘画流程图时进行相关验证。"},
-          {src:'./static/img/examineAndPpprove.jpg',link:"http://www.raoqingjia.online/examineAndPpprove/index.html",title:"h5嵌套页面",info:"web-view嵌套h5页面"},
-          {src:'./static/img/osh5.jpg',link:"https://www.raoqingjia.online/osh5/index.html",title:"h5宣传页面",info:"利用rem换算比例适应各移动平台，使用annimate.css实现动画"},
-          {src:'./static/img/osweb.png',link:"https://www.raoqingjia.online/osDemo/index.html",title:"vue3.0项目",info:"vue3.0项目搭建，使用ant design vue的UI框架，利用mockjs创建临时数据调取，使用了dayjs、lodash、qrcanvas等三方库，利用封装了axios请求方式，将数据请求模块化，实现了前后台数据整体加密"},
-        ],
+        imgsArr: [],
         fetchImgsData:[]
       }
     },
     mounted() {
-      this.$nextTick(function () {
-         setTimeout(function(){
-             $(".img-wraper").css("width","100%");
-             $(".img-wraper").css("height","100%");
-
-         },50)
-      })
+      this.imgsArr = [   //数组数量小于5个也就是小于一行的数量，会报Cannot read property 'style' of undefined错误
+        {src:'./static/img/ecweb.jpg',link:"/ecWebTest/src/app/index.html",title:"angular1.0项目",info:"基于angular-1.2.9搭建，兼容IE8、利用Bootstrap实现响应式、BootstrapDatepicker日期插件、Echart图表、DataTables表单排序、kkpager页面排序功能"},
+        {src:'./static/img/salss-app.jpg',link:"/salssVue/index.html",title:"H5移动端项目",info:"基于Vue2.0,利用vue全家桶,未使用UI框架，利用rem换算比例适应各移动平台,mobileSelect筛选插件,swiper3.0滑屏,Echart图表"},
+        {src:'./static/img/bpm_web_new.jpg',link:"/bpm_web_new/flow.html",title:"流程图",info:"设计流程图（raphael）源码myflow.js，修改了删除方式，自定义了流程步骤，保存绘画流程图时进行相关验证。"},
+        {src:'./static/img/examineAndPpprove.jpg',link:"/examineAndPpprove/index.html",title:"h5嵌套页面",info:"web-view嵌套h5页面"},
+        {src:'./static/img/osh5.jpg',link:"/osh5/index.html",title:"h5宣传页面",info:"利用rem换算比例适应各移动平台，使用annimate.css实现动画"},
+        {src:'./static/img/osweb.png',link:"/osDemo/index.html",title:"vue3.0项目",info:"vue3.0项目搭建，使用ant design vue的UI框架，利用mockjs创建临时数据调取，使用了dayjs、lodash、qrcanvas等三方库，利用封装了axios请求方式，将数据请求模块化，实现了前后台数据整体加密"},
+        {src:'./static/img/absweb.png',link:"/absweb/index.html",title:"Angular8项目",info:"Angular8项目搭建，引用ng-zorro-antdUI框架、echarts图例、typescript等搭建的大数据量B端项目"},
+      ]
     },
     computed: {
       caseNav(){
@@ -77,18 +46,31 @@
       urlFun(code){
         this.$router.push({path: "/list"});
         this.$store.commit('listTabFun_m', code)
+      },
+      clickFn(event, { index, value }){
+        // 只有当点击到图片时才进行操作
+        if (event.target.tagName.toLowerCase() == 'img') {
+          console.log(window.location)
+          window.open(window.location.origin + value.link);
+        }
       }
     }
   }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
   .vue-waterfall-easy .img-box .img-inner-box img{
     padding: 5px;
     box-sizing: border-box;
   }
+  .img-info{
+     background-color: #FFFFFF;
+    /*padding: 10px;*/
+  }
   .img-info h3{
     font-size: 16px;
+    line-height: 26px;
     text-align: left;
     color: #000000;
     word-break:break-all; /*只对英文起作用，以字母作为换行依据*/
@@ -97,6 +79,7 @@
   }
   .img-info p{
     text-align: left;
+    line-height: 22px;
     color: #000000;
     word-break:break-all; /*只对英文起作用，以字母作为换行依据*/
     word-wrap:break-word; /*只对英文起作用，以单词作为换行依据*/
