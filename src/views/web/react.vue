58458e3711f5532c3979ed44e175b441e13b5cad
raoqingjia <1007804453@qq.com>
2022-08-20 14:11:49 +0800

    新增react笔记

--- src/views/web/react.vue
+++ src/views/web/react.vue
@@ -563,7 +563,9 @@ class Btn extends React.Component {
   render() {
     return < button onClick={() => this.showBtn()}>按钮< /button>;
   }
-}</pre>
+}
+
+          https://blog.csdn.net/IT_10/article/details/91493425</pre>
           <h3>ref 获取dom的三种方式</h3>
           <pre>在React组件并不是真实的 DOM 节点，而是存在于内存之中的一种数据结构，叫做虚拟 DOM （virtual DOM）。只有当它插入文档以后，才会变成真实的 DOM 。根据 React 的设计，所有的 DOM 变动，都先在虚拟 DOM 上发生，然后再将实际发生变动的部分，反映在真实 DOM上，这种算法叫做 DOM diff，它可以极大提高网页的性能表现。但是，有时需要从组件获取真实 DOM 的节点，这时就要用到 ref 属性
 方式一：字符串方式（不推荐）
