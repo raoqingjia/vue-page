c4c479b9af20dc37810e10ad32e6b342694ac1ef
raoqingjia <10078044533@qq.com>
2022-10-20 22:49:20 +0800

    更新 :key="index"

--- src/views/crm/activeMq.vue
+++ src/views/crm/activeMq.vue
@@ -8,7 +8,7 @@
         </p>
         <div class="art-content">
           <ul class="catalogue clearfix">
-            <li v-for="(items,index) in catalogue"><a @click="jump(index)">{{items.name}}</a></li>
+            <li v-for="(items,index) in catalogue" :key="index" ><a @click="jump(index)">{{items.name}}</a></li>
           </ul>
           <h3>linux下activemq安装与配置</h3>
           <pre>
--- src/views/crm/docker.vue
+++ src/views/crm/docker.vue
@@ -8,7 +8,7 @@
         </p>
         <div class="art-content">
           <ul class="catalogue clearfix">
-            <li v-for="(items,index) in catalogue"><a @click="jump(index)">{{items.name}}</a></li>
+            <li v-for="(items,index) in catalogue"  :key="index" ><a @click="jump(index)">{{items.name}}</a></li>
           </ul>
           <h3>docker常用命令</h3>
           <pre>docker images | grep vueweb     查找vueweb开头的镜像名称
