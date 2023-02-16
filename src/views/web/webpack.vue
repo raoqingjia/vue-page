e00a2dd45465398e1cd49389730ae50cb0c46af6
raoqingjia <10078044533@qq.com>
2023-02-17 01:16:48 +0800

    更新webpack

--- src/views/web/webpack.vue
+++ src/views/web/webpack.vue
@@ -12,32 +12,61 @@
               <a @click="jump(index)" >{{ index + 1 }}、{{ items.name }}</a>
             </li>
           </ul>
-          <h3>webpack 五个核心概念</h3>
-          <pre>
+          <h3>webpack 核心字段概念</h3>
+          <pre>module.exports={
+    entry:{
+        main:"./src/app.js"
+    },
+    output:{
+        path:path.resolve(__dirname,"app"),
+        publicPath:"/",
+        filename:"[name].js"
+     },
+    plugins: [ ],
+    mode: 'development'
+}
 1、Entry
 入口(Entry)指示 webpack 以哪个文件为入口起点开始打包，分析构建内部依赖图。
+
 2、Output
 输出(Output)指示 webpack 打包后的资源 bundles 输出到哪里去，以及如何命名。
+    path:打包后的文件存放的地方
+    publicPath：指定资源文件引用的目录
+    [name]:这里app
+    __dirname:是node.js中的一个全局变量，它指向当前执行脚本所在的目录
+
 3、Plugins
 插件(Plugins)可以用于执行范围更广的任务。插件的范围包括，从打包优化和压缩，一直到重新定义环境中的变量等。
+
 4、Mode
 模式(Mode)指示 webpack 使用相应模式的配置。
 development 会将 DefinePlugin 中 process.env.NODE_ENV 的值设置为 development。启用 NamedChunksPlugin 和NamedModulesPlugin。能让代码本地调试运行的环境
-production  会将 DefinePlugin 中 process.env.NODE_ENV 的值设置为 production。启用 FlagDependencyUsagePlugin, FlagIncludedChunksPlugin, ModuleConcatenationPlugin, NoEmitOnErrorsPlugin, OccurrenceOrderPlugin, SideEffectsFlagPlugin 和 TerserPlugin。能让代码优化上线运行的</pre
-          >
+production  会将 DefinePlugin 中 process.env.NODE_ENV 的值设置为 production。启用 FlagDependencyUsagePlugin, FlagIncludedChunksPlugin, ModuleConcatenationPlugin, NoEmitOnErrorsPlugin, OccurrenceOrderPlugin, SideEffectsFlagPlugin 和 TerserPlugin。能让代码优化上线运行的</pre>
           <h3>输入指令下载并安装 webpack</h3>
-          <pre>
-npm install webpack webpack-cli -g
+          <pre>npm install webpack webpack-cli -g
 npm install webpack webpack-cli -D
 npm i css-loader style-loader less-loader less -D   打包样式资源下载安装 loader 包
 npm install --save-dev html-webpack-plugin  打包 HTML 资源
 npm install --save-dev html-loader url-loader file-loader   打包图片资源
 npm install webpack-dev-server 安装webpack-dev-server实现了自动编译刷新浏览器  运行指令: npx webpack-dev-server
-npm install --save-dev mini-css-extract-plugin   提取 css 成单独文件</pre
-          >
+npm install --save-dev mini-css-extract-plugin   提取 css 成单独文件</pre>
+          <h3>webpack命令配置</h3>
+          <pre>webpack --progress --colors  当项目逐渐变大，webpack 的编译时间会变长，可以通过参数让编译的输出内容带有进度和颜色
+webpack --display-error-details  方便出错时能查阅更详尽的信息
+webpack --config XXX.js   使用另一份配置文件（比如webpack.config2.js）来打包
+webpack --watch    监听变动并自动打包,开启监听模式后，没有变化的模块会在编译后缓存到内存中，而不会每次都被重新编译
+webpack -p   压缩混淆脚本，这个非常非常重要！
+webpack -d   生成map映射文件，告知哪些模块被最终打包到哪里了
+
+开启webpack-dev-server服务，http://localhost:8080/webpack-dev-server/ 可以浏览项目中的页面和编译后的资源输出，并且通过一个 socket.io 服务实时监听它们的变化并自动刷新页面。
+实时监听是有条件的：
+1、所有的文件在同一个目录下
+2、简单css可以，但是css预编译器sass就做不到实时监控
+webpack --display-error-details//打印错误日志
+
+          </pre>
           <h3>编译打包应用</h3>
-          <pre>
-webpack 能够编译打包 js 和 json 文件。能将 es6 的模块化语法转换成浏览器能识别的语法。能压缩代码。不能编译打包 css、img 等文件。不能将 js 的 es6 基本语法转化为 es5 以下语法。
+          <pre>webpack 能够编译打包 js 和 json 文件。能将 es6 的模块化语法转换成浏览器能识别的语法。能压缩代码。不能编译打包 css、img 等文件。不能将 js 的 es6 基本语法转化为 es5 以下语法。
 webpack 开发环境的基本配置
 src 是项目文件用的是ES6的写法  webpack.config 是打包配置用的是common.js写法
 
@@ -133,11 +162,9 @@ module.exports = {
     // 自动打开浏览器
     open: true
   }
-} </pre
-          >
+} </pre>
           <h3>对css的处理</h3>
-          <pre>
-对css的处理主要在于1、提取css成单独文件2、css兼容性处理3、压缩css
+          <pre>对css的处理主要在于1、提取css成单独文件2、css兼容性处理3、压缩css
 webpack中对css处理要使用css-loader/style-loader/postcss-loader
 webpack是只识别js的，所以我们要将css转换成js中的代码，这时转换就需要用到css-loader style-loade
 npm install --save-dev css-loader style-loader
@@ -281,17 +308,78 @@ module.exports = {
     ]
   },
   ........
-};</pre
-          >
-          <h3>webpack配置之source-map 控制台报错信息是否明确和这个息息相关</h3>
+};</pre>
+          <h3>process.env.NODE_ENV详解</h3>
+          <pre>在node中，有全局变量process表示的是当前的node进程。process.env包含着关于系统环境的信息。但是process.env中并不存在NODE_ENV这个东西。NODE_ENV是用户一个自定义的变量，在webpack中它的用途是判断生产环境或开发环境的依据的。
+为了查看 process的基本信息，我们可以在文件夹中 新建一个 process.js 文件，在里面加一句代码：console.log(process);然后进入该文件夹，执行 node process.js 可以在命令行中打印信息,里面的相应信息相当的多
+我打印了一个console.log(process.env.NODE_ENV)这个字段实际是在node中不存在的，这个需要自己单独声明
+
+理解 DefinePlugin 含义
+官网配置地址  https://webpack.js.org/plugins/define-plugin/#usage
+官网解释的是：DefinePlugin允许我们创建全局变量，可以在编译时进行设置，因此我们可以使用该属性来设置全局变量来区分开发环境和正式环境。这就是 DefinePlugin 的基本功能。
+因此我们可以在 webpack.config.js 中添加如下代码配置全局变量信息了，因为当webpack进行编译的时候会全局设置变量；如下代码：
+module.exports = {
+  plugins: [
+    // 设置环境变量信息
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(true),
+      VERSION: JSON.stringify('1.1.0'),
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ]
+}
+package.json 打包配置如下命令：
+"scripts": {
+  "dev": "webpack-dev-server --progress --colors --devtool cheap-module-eval-source-map --hot --inline",
+  "build": "webpack --progress --colors --devtool cheap-module-source-map",
+  "build:dll": "webpack --config webpack.dll.config.js"
+}
+这样配置完成后，为了验证一下是否是全局变量，运行 npm run dev 打包后，我们可以在我们项目中入口js文件，打印下即可：比如如下代码：
+console.log('Running App version ' + VERSION); // 打印 Running App version 1.1.0
+console.log(PRODUCTION); // 打印 true
+console.log(process.env); // 打印 { NODE_ENV: undefined }
+如上信息可以看到 process.env.NODE_ENV 打印出为 undefined，那是因为我们在 package.json 文件中未进行配置。下面我们把 package.json 加上 NODE_ENV 变量值，代码打包命令变成如下：
+"scripts": {
+  "dev": "NODE_ENV=development webpack-dev-server --progress --colors --devtool cheap-module-eval-source-map --hot --inline",
+  "build": "NODE_ENV=production webpack --progress --colors --devtool cheap-module-source-map",
+  "build:dll": "webpack --config webpack.dll.config.js"
+}
+如上打包命令，在dev打包命令上，前面加上了 NODE_ENV=development 命令，在build打包命令上前面加上了 NODE_ENV=production，因此继续查看代码结果变为如下：
+console.log(process.env); // 打印 { NODE_ENV: 'development' }
+复制代码可以看到这个时候 process.env.NODE_ENV  才有值，因此在项目打包中，为了区分开发环境和正式环境我们像如上配置即可，然后在 webpack.config.js 中通过 process.env.NODE_ENV  这个来区分正式环境还是开发环境即可。
+
+
+
+webpack-cli提供的参数 node-env 官网配置地址  https://webpack.js.org/api/cli/#node-env
+
+
+          </pre>
+          <h3>配置source-map打印控制台报错信息</h3>
           <pre>https://webpack.docschina.org/configuration/devtool/ 官方介绍
-Devtool作用
+Devtool作用：
 此选项控制是否生成，以及如何生成 source map。
 使用 SourceMapDevToolPlugin 进行更细粒度的配置。查看 source-map-loader 来处理已有的 source map
 
+devtool选项有这么几个：
+source-map、cheap-module-source-map、eval-source-map、cheap-module-eval-source-map
+
+source-map
+在一个单独的文件中产生一个完整且功能完全的文件。这个文件具有最好的source map，但是它会减慢打包速度；
+
+cheap-module-source-map
+在一个单独的文件中生成一个不带列映射的map，不带列映射提高了打包速度，但是也使得浏览器开发者工具只能对应到具体的行，不能对应到具体的列（符号），会对调试造成不便；
+
+eval-source-map
+使用eval打包源文件模块，在同一个文件中生成干净的完整的source map。这个选项可以在不影响构建速度的前提下生成完整的sourcemap，但是对打包后输出的JS文件的执行具有性能和安全的隐患。在开发阶段这是一个非常好的选项，在生产阶段则一定不要启用这个选项；
+
+cheap-module-eval-source-map
+这是在打包文件时最快的生成source map的方法，生成的Source Map 会和打包后的JavaScript文件同行显示，没有列映射，和eval-source-map选项具有相似的缺点；
+
 source-map是一种提供源代码到构建后代码映射技术，也就是说如果构建后代码出错了，通过映射可以追踪源代码错误
 格式：[inline- | hidden- | eval-][nosources-][cheap-[module-]]source-map
-  可以任意排列，但[]的顺序不能乱
+可以任意排列，但[]的顺序不能乱
 具体介绍
 source-map: 在外部生成一个文件
   在控制台会显示 错误代码准确信息 和 源代码的错误位置
@@ -314,63 +402,112 @@ cheap-odule-source-map: 外部
   module会将loader的source-map加入
 
 内嵌与外部的区别： 1.外部生成单独的文件，内嵌没有 2.内嵌构建速度快
-
 这么多source-map如何选择？
 开发环境：速度快，调试更友好
-  速度快（ eval>inline>cheap>··· ）
-    组合eval-cheap-source-map > eval-source-map
-  调试更友好
-    source-map > cheap-module-source-map > cheap-source-map
-  最终结果：cheap-module-source-map 和 eval-source-map (vuecli与react脚手架默认)
+速度快（ eval>inline>cheap>··· ）
+  组合eval-cheap-source-map > eval-source-map
+调试更友好
+  source-map > cheap-module-source-map > cheap-source-map
+最终结果：cheap-module-source-map 和 eval-source-map (vuecli与react脚手架默认)
 
 生产环境：源代码要不要隐藏？调试要不要更友好
-  内嵌会让代码体积变大，所以在生产环境下不用内嵌
-  nosources-source-map  全部隐藏
-  hidden-source-map   只隐藏源代码，会提示构建后代码错误信息
-  最终结果：source-map 和 cheap-module-source-map
+内嵌会让代码体积变大，所以在生产环境下不用内嵌
+nosources-source-map  全部隐藏
+hidden-source-map   只隐藏源代码，会提示构建后代码错误信息
+最终结果：source-map 和 cheap-module-source-map
 
-webpack.config.js文件全部配置
-const { resolve } = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+建议
+中小型项目开发阶段使用eval-source-map,大项目里使用cheap-module-eval-source-map
 
+配置
 module.exports = {
-  entry: ['./src/index.js', './src/index.html'],
-  output: {
-    filename: 'bundle.js',
-    path: resolve(__dirname, 'bundle')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      }
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
-  mode: 'development',
-  devServer: {
-    contentBase: resolve(__dirname, 'bundle'),
-    compress: true,
-    port: 3000,
-    open: true,
-    hot: true,  // 启用HMR模块
-  },
   devtool: 'cheap-module-source-map'
+  .....
 }</pre>
-          <h3></h3>
-          <pre></pre>
+          <h3>本地服务器devServer</h3>
+          <pre>在开发模式下，DevServer 提供虚拟服务器，让我们进行开发和调试,需要单独安装
+命令
+npm install webpack-dev-server --save-dev
+使用
+let webpackConfig =require('./webpack.config')
+webpackConfig.devServer = {
+  ...
+}
+详细介绍
+1、hot
+热模块更新作用。即修改或模块后，保存会自动更新，页面不用刷新呈现最新的效果。
+这不是和 webpack.HotModuleReplacementPlugin （HMR） 这个插件不是一样功能吗？是的，不过请注意了，HMR 这个插件是真正实现热模块更新的。而 devServer 里配置了 hot: true , webpack会自动添加 HMR 插件。所以模块热更新最终还是 HMR 这个插件起的作用。
+2、host
+写主机名的。默认 localhost
+3、inline
+设置为true，当源文件改变时会自动刷新页面
+4、port
+端口号。默认 8080
+5、historyApiFallback
+如果为 true ，页面出错不会弹出 404 页面
+6、compress
+如果为 true ，开启虚拟服务器时，为你的代码进行压缩。加快开发流程和优化的作用
+7、contentBase
+建议绝对路径 .你要提供哪里的内容给虚拟服务器用
+// 单目录
+contentBase: path.join(__dirname, "public")
+// 多目录
+contentBase: [path.join(__dirname, "public"), path.join(__dirname, "assets")]
+默认情况下，它将使用您当前的工作目录来提供内容
+8、open
+true，则自动打开浏览器
+9、overlay
+如果为 true ，在浏览器上全屏显示编译的errors或warnings。默认 false （关闭）
+如果你只想看 error ，不想看 warning
+overlay：{
+  errors：true，
+  warnings：false
+}
+10、quiet
+true，则终端输出的只有初始启动信息。 webpack 的警告和错误是不输出到终端的
+11、publicPath
+配置资源文件路径
+publicPath: "/assets/"
+// 原本路径 --> 变换后的路径
+http://localhost:8080/app.js --> http://localhost:8080/assets/app.js
+12、proxy
+当您有一个单独的API后端开发服务器，并且想要在同一个域上发送API请求时，则代理这些 url
+proxy: {
+    '/proxy': {
+        target: 'http://your_api_server.com',
+        changeOrigin: true,
+        pathRewrite: {
+            '^/proxy': ''
+        }
+  }
+假设你主机名为 localhost:8080 , 请求 API 的 url 是 http：//your_api_server.com/user/list
+'/proxy'：如果点击某个按钮，触发请求 API 事件，这时请求 url 是http：//localhost:8080/proxy/user/list
+changeOrigin：如果 true ，那么 http：//localhost:8080/proxy/user/list 变为 http：//your_api_server.com/proxy/user/list 。但还不是我们要的 url
+pathRewrite：重写路径。匹配 /proxy ，然后变为” ，那么 url 最终为 http：//your_api_server.com/user/list
+
+完整配置如下:
+devServer: {
+        contentBase: './app',
+        port: 8081,
+        inline: true,
+        open: true,
+        openPage: '',
+        hot: true,
+        proxy: {
+            "/api/*": {
+                target: ip,
+                secure: false,
+                changeOrigin: true,
+                pathRewrite: {
+                    "/api": "/"
+                }
+            }
+        },
+        overlay: {
+            warnings: true,
+            errors: true
+        }
+    }</pre>
         </div>
       </div>
     </div>
@@ -387,7 +524,9 @@ export default {
       catalogue: [],
     };
   },
-  created() {},
+  created() {
+    // console.log(process);
+  },
   mounted: function () {
     this.$nextTick(function () {
       this.createCatalogue();
@@ -396,16 +535,6 @@ export default {
   computed: {},
   methods: {
     jump(index) {
-      //        let jump = document.getElementsByTagName('h3');
-      //       // 获取需要滚动的距离
-      //        let total = jump[index].offsetTop;
-      //        // Chrome
-      //        document.body.scrollTop = total;
-      //        // Firefox
-      //        document.documentElement.scrollTop = total;
-      //       // Safari
-      //        window.pageYOffset = total
-      //        https://www.cnblogs.com/wisewrong/p/6495726.html  参考网站
       let jump = document.getElementsByTagName("h3");
       let total = jump[index].offsetTop; // 获取目标位置滚动的距离
       let distance =
