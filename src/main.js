22fac8f557372f67281822c4aacd6daba527805e
raoqingjia <1007804453@qq.com>
2023-07-05 20:49:15 +0800

    更新直接复制下面代码就能使用

--- src/main.js
+++ src/main.js
@@ -75,3 +75,31 @@ new Vue({
   components: {App},
   template: '<App/>'
 })
+
+
+
+// function addLink() {
+//   var body_element = document.body;
+//   var selection;
+//   var blogName = 'raoqingjia 日常笔记';
+//   selection = window.getSelection() ? window.getSelection() : document.selection.createRange().text;
+//   if (window.clipboardData) {
+//     var pagelink ="<br/>---------------------<br/>本文来源："+blogName+"<br/>原文链接："+document.location.href+"";
+//     var copyText = selection + pagelink;
+//     window.clipboardData.setData ("Text", copyText);
+//     return false;
+//   } else {
+//     var pagelink = "<br/>---------------------<br/>本文来源："+blogName+"<br/>原文链接："+document.location.href+"";
+//     var copyText = selection + pagelink;
+//     var newDiv = document.createElement('div');
+//     newDiv.style.position ='absolute';
+//     newDiv.style.left ='-99999px';
+//     body_element.appendChild(newDiv);
+//     newDiv.innerHTML = copyText;
+//     selection.selectAllChildren(newDiv);
+//     window.setTimeout(function() {
+//       body_element.removeChild(newDiv);
+//     },0);
+//   }
+// }
+// document.oncopy = addLink;
